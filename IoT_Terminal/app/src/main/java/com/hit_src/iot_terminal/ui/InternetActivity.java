edc3d3928b95280bb3f1f7317b7ae41535b45d56
shao0099876 <shao0099876@outlook.com>
2020-02-25 13:23:00 +0800

    AS update backup

---
+++ IoT_Terminal/app/src/main/java/com/hit_src/iot_terminal/ui/InternetActivity.java
@@ -0,0 +1,16 @@
+package com.hit_src.iot_terminal.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hit_src.iot_terminal.R;
+
+public class InternetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_internet);
+    }
+}
