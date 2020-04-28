25de544174a7c76b3656a9476ee195fc7b38bb29
shao0099876 <shao0099876@outlook.com>
2020-04-28 10:03:33 +0800

    重构SensorAddFragment

--- IoT_Terminal/app/src/main/java/com/hit_src/iot_terminal/ui/sensor/SensorAddFragment.java
+++ IoT_Terminal/app/src/main/java/com/hit_src/iot_terminal/ui/sensor/SensorAddFragment.java
@@ -1,34 +1,29 @@
 package com.hit_src.iot_terminal.ui.sensor;
 
 import android.os.Bundle;
-import android.os.RemoteException;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
+import android.widget.BaseAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.hit_src.iot_terminal.GlobalVar;
-import com.hit_src.iot_terminal.MainApplication;
+import com.hit_src.iot_terminal.MainActivity;
 import com.hit_src.iot_terminal.R;
 import com.hit_src.iot_terminal.object.sensortype.SensorType;
-import com.hit_src.iot_terminal.service.DatabaseService;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
 
 public class SensorAddFragment extends Fragment {
     private Spinner typeSpinner;
-    private EditText loraAddrEditText;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -40,50 +35,61 @@ public class SensorAddFragment extends Fragment {
     public void onStart() {
         super.onStart();
 
-        View view = getView();
+        final View view = getView();
+        assert view != null;
         typeSpinner = view.findViewById(R.id.Sensor_Type_Spinner);
-        loraAddrEditText = view.findViewById(R.id.Sensor_Add_LoraAddr_EditText);
-        Button confirmButton = view.findViewById(R.id.Sensor_Add_Button);
-
-        confirmButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (loraAddrEditText.getText().toString() == null || loraAddrEditText.getText().toString().isEmpty()) {
-                    Toast.makeText(getContext(), "LoRa地址不能为空", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                String datatype = (String) typeSpinner.getSelectedItem();
-                for (int i : GlobalVar.sensorTypeHashMap.keySet()) {
-                    SensorType now = GlobalVar.sensorTypeHashMap.get(i);
-                    if (datatype.equals(now.name)) {
-                        DatabaseService.getInstance().addSensor(i, Integer.valueOf(loraAddrEditText.getText().toString()));
-                        break;
-                    }
-                }
-                FragmentManager manager = getParentFragmentManager();
-                FragmentTransaction transaction = manager.beginTransaction();
-                transaction.remove(SensorAddFragment.this);
-                transaction.commit();
-            }
-        });
-        Set<String> set = new HashSet<>();
-        for (int i : GlobalVar.sensorTypeHashMap.keySet()) {
-            set.add(GlobalVar.sensorTypeHashMap.get(i).name);
-        }
-        Object[] tmp = set.toArray();
-        String[] data = new String[tmp.length];
-        for (int i = 0; i < data.length; i++) {
-            data[i] = (String) tmp[i];
-        }
-        final ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(getContext(), R.layout.spinner_display_style, data);
-        spinnerAdapter.setDropDownViewResource(R.layout.spinner_dropdown_style);
-        getActivity().runOnUiThread(new Runnable() {
+        MainActivity.self.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                typeSpinner.setAdapter(spinnerAdapter);
+                typeSpinner.setAdapter(new SensorTypeAdapter(GlobalVar.getSensorTypeList()));
                 typeSpinner.setSelection(0);
             }
         });
+        view.findViewById(R.id.Sensor_Add_Button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText loraAddrEditText = view.findViewById(R.id.Sensor_Add_LoraAddr_EditText);
+                if (loraAddrEditText.getText().toString().isEmpty()) {
+                    Toast.makeText(getContext(), "LoRa地址不能为空", Toast.LENGTH_SHORT).show();
+                } else {
+                    SensorType sensorType = (SensorType) typeSpinner.getSelectedItem();
+                    GlobalVar.addSensor(sensorType, Integer.parseInt(loraAddrEditText.getText().toString()));
+                    FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+                    transaction.remove(SensorAddFragment.this);
+                    transaction.commit();
+                }
+            }
+        });
+    }
+}
 
+class SensorTypeAdapter extends BaseAdapter {
+    private ArrayList<SensorType> sensorTypes;
+
+    public SensorTypeAdapter(ArrayList<SensorType> p) {
+        sensorTypes = p;
+    }
+
+    @Override
+    public int getCount() {
+        return sensorTypes.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return sensorTypes.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = View.inflate(MainActivity.self, R.layout.spinner_display_style, null);
+        TextView textView = view.findViewById(R.id.Spinner_TextView);
+        textView.setText(sensorTypes.get(position).name);
+        return view;
     }
 }
